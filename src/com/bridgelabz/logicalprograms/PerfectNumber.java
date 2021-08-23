2d3a683cf7f2d47ed7b02b191c498cb2dc839e1d
Aravindhan V <aravindhan.v@perfios.com>
2021-08-23 22:16:37 +0530

    ADDED: Check if number is perfect or not

---
+++ src/com/bridgelabz/logicalprograms/PerfectNumber.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.logicalprograms;
+
+public class PerfectNumber {
+	public static void main(String[] args) {
+
+		int sumOfDivisors = 0;
+		int number = 256;
+
+		for (int index = 2; index * index <= number; index++) {
+			if (number % index == 0) {
+				sumOfDivisors += (index + (number/index));
+			}
+		}
+		sumOfDivisors+=1;
+		
+		if (sumOfDivisors == number) {
+			System.out.println(number + " is a perfect Number");
+		} else {
+			System.out.println(number + " is not a Perfect Number");
+		}
+
+	}
+}
